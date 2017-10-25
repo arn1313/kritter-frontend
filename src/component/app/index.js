bac187acaa80319b637657a746950b4a9de2ed39
GavinThomas1192 <gthomas1192@gmail.com>
2017-10-25 15:32:55 -0700

    yolo

--- src/component/app/index.js
+++ src/component/app/index.js
@@ -17,20 +17,18 @@ class App extends React.Component {
 
 
   componentWillMount() {
-    let token = utils.cookieFetch('X-Kritter-Token');
-    // let token = localStorage.getItem('reduxPersist:auth');
+    // let token = utils.cookieFetch('X-Kritter-Token');
+    let token = JSON.parse(localStorage.getItem('reduxPersist:auth'));
     // console.log('kashdjghasopiughasoighaoighaoig',token);
     if(token) {
       this.props.tokenSet(token)
         .then(() => this.props.userFetch())
-        .then(result => console.log('After userFetch', result.body))
+        // .then(result => console.log('After userFetch', result.body))
         .then(() => this.props.postFetch())
-        .then(() => this.props.redirect('/home'))
-
         .catch(console.error);
     }
-    if(!this.props.user)this.props.userFetch();
-    if(!this.props.post)this.props.postFetch();
+    // if(!this.props.user) this.props.userFetch();
+    // if(!this.props.post) this.props.postFetch();
   }
 
   render() {
